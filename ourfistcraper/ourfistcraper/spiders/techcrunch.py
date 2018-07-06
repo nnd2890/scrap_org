ebfe30c036306216777689b3f532ee6132ca40fa
nnd2890 <dainnd00258@fpt.edu.vn>
2018-07-06 18:07:46 +0700

    Web Scraping in Python using Scrapy (with multiple examples) by Mohd Sanad Zaki Rizvi (July 2017)

---
+++ ourfistcraper/ourfistcraper/spiders/techcrunch.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class TechcrunchSpider(scrapy.Spider):
+    name = 'techcrunch'
+    allowed_domains = ['techcrunch.com/feed/']
+    start_urls = ['http://techcrunch.com/feed//']
+
+    custom_settings = {
+        'FEED_URI': 'tmp/techcrunch.csv'
+    }
+
+    def parse(self, response):
+        # remove XML namespaces
+        response.selector.remove_namespaces()
+
+        titles = response.xpath('//item/title/text()').extract()
+        authors = response.xpath('//item/creator/text()').extract()
+        dates = response.xpath('//item/pubDate/text()').extract()
+        links = response.xpath('//item/link/text()').extract()
+
+        for item in zip(titles, authors, dates, links):
+            scraped_infor = {
+                'title':item[0],
+                'author':item[1],
+                'publish_date':item[2],
+                'link':item[3],
+            }
+
+            yield scraped_infor
