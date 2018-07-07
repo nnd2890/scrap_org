507bfa151611a741c8f7692335f47b42c300ccbe
nnd2890 <dainnd00258@fpt.edu.vn>
2018-07-07 15:44:04 +0700

    done but not run

--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -64,9 +64,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'stack.pipelines.StackPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'stack.pipelines.MongoDBPipeline'
+}
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
+MONGODB_DB = 'stackoverflow'
+MONGODB_COLLECTION = 'questions'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
